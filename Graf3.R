18568f0968bb6e75462b723f0059a304435b0491
Oliab <oliabherrera@gmail.com>
2017-07-11 13:24:21 -0500

    agrego gràfica de barras horizontal

--- Graf3.R
+++ Graf3.R
@@ -9,15 +9,23 @@ colnames(proy)[1]<-"Sector"
 
 top_labels <- c('Conclusion', 'Contratacion', 'Ejecucion', 'Planeacion')
 
-ejec<-  plot_ly(proy, x = ~Sector, y = ~Planeación, type = 'bar', name = 'Planeacion', marker = list(color = 'rgb(49,130,189)')) %>%
-  add_trace(y = ~Ejecución, name = 'Ejecucion', marker = list(color = 'rgb(204,204,204)')) %>%
-  add_trace(y = ~Contratación, name = 'Contratacion', marker = list(color = 'rgb(28,208,204)')) %>%  
-  add_trace(y = ~Conclusión, name = 'Conclusion', marker = list(color = 'rgb(254,204,204)')) %>%
+ejec<-  plot_ly(proy, x = ~Planeación, y = ~Sector, type = 'bar', name = 'Planeacion', marker = list(color = 'rgb(49,130,189)'), orientation="h") %>%
+  add_trace(x = ~Ejecución, name = 'Ejecucion', marker = list(color = 'rgba(222,45,38,0.8)')) %>%
+  add_trace(x = ~Contratación, name = 'Contratacion', marker = list(color = 'rgb(28,208,204)')) %>%  
+  add_trace(x = ~Conclusión, name = 'Conclusion', marker = list(color = 'rgb(345,204,204)')) %>%
     
-  layout(xaxis = list(title = "", tickangle = -45),
+  layout(title = 'Número de proyectos por sector',
+        xaxis = list(title = "", tickangle = -45),
          yaxis = list(title = ""),
          margin = list(b = 100),
-         barmode = 'group')
+         barmode = 'group',
+         margin=list(
+           l = 550,
+           r = 50,
+           b = 100,
+           t = 100,
+           pad = 4
+        ))
   
   link_ejec = api_create(ejec, filename = "Aga-Ejec")
   
